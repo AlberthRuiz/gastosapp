9f2868162db8b04895a7bfdac2f793c8b234dfc9
Christiam Mendoza <ch.alb.rz@hotmail.com>
2024-02-10 09:40:08 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/gastosapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gastosapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:gastosapp/widgets/item_gasto_widget.dart';
+import 'package:gastosapp/widgets/textfield_normal_widget.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    print("presionado");
+                  },
+                  child: Container(
+                    alignment: Alignment.bottomCenter,
+                    color: Color(0xff101321),
+                    width: double.infinity,
+                    height: 120,
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 24),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.add,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            "Agregar",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(34),
+                        bottomRight: Radius.circular(34),
+                      ),
+                      color: Colors.white,
+                    ),
+                    child: SingleChildScrollView(
+                      physics: BouncingScrollPhysics(),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Resumen de gastos",
+                              style: TextStyle(
+                                  fontSize: 28, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              "Gestiona tus gastos de la mejor forma",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.black45),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 16),
+                              child: TextFieldNormal(),
+                            ),
+                            ItemGastoWidget(),
+                            ItemGastoWidget(),
+                            ItemGastoWidget(),
+                            ItemGastoWidget(),
+                            ItemGastoWidget(),
+                            ItemGastoWidget(),
+                            ItemGastoWidget(),
+                            ItemGastoWidget(),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 75,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
